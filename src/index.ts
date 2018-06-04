73332220d5750e4832613908968310426d1a3b11
Paul Blanche <blanchepaule@whiteshoulders.fr>
2018-06-04 19:53:45 +0200

    Export Validator type

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ export { number } from './codec/number';
 export { object } from './codec/object';
 export { optional } from './codec/optional';
 export { string } from './codec/string';
-export { union2, union3, union4, union5 } from './codec/union';
+export { union } from './codec/union';
 export { value } from './codec/value';
+
+export { Validator } from './types';
